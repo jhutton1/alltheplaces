5e18d47ca7a62e018520f975f2bc7fe71a3004e2
jhutton1 <jaimehutton01@gmail.com>
2023-10-02 12:38:38 -0400

    added category department store

--- locations/spiders/macys_backstage.py
+++ locations/spiders/macys_backstage.py
@@ -1,10 +1,11 @@
 from scrapy.spiders import SitemapSpider
 
+from locations.categories import Categories
 from locations.structured_data_spider import StructuredDataSpider
 
 
 class MacysBackstageSpider(SitemapSpider, StructuredDataSpider):
     name = "macys_backstage"
-    item_attributes = {"brand": "Macy's Backstage"}
+    item_attributes = {"brand": "Macy's Backstage", "brand_wikidata": "Q122914589", "extras": Categories.SHOP_DEPARTMENT_STORE.value}
     sitemap_urls = ["https://stores.macysbackstage.com/sitemap.xml"]
     wanted_types = ["DepartmentStore"]
