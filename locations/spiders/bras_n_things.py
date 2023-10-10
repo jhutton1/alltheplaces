55ed41bf0f44f764ee8cdac468b01b5a2b349842
jhutton1 <jaimehutton01@gmail.com>
2023-10-10 11:03:16 -0400

    added category clothes

--- locations/spiders/bras_n_things.py
+++ locations/spiders/bras_n_things.py
@@ -1,9 +1,10 @@
+from locations.categories import Categories
 from locations.storefinders.limesharp_store_locator import LimesharpStoreLocatorSpider
 
 
 class BrasNThingsSpider(LimesharpStoreLocatorSpider):
     name = "bras_n_things"
-    item_attributes = {"brand": "Bras N Things", "brand_wikidata": "Q120669960"}
+    item_attributes = {"brand": "Bras N Things", "brand_wikidata": "Q120669960", "extras": Categories.SHOP_CLOTHES.value}
     allowed_domains = [
         # All of these allowed domains return the same store data.
         "www.brasnthings.com",
