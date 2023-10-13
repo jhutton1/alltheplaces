4499f7549dedf96f0ea1548b67fefa6d3b004157
jhutton1 <jaimehutton01@gmail.com>
2023-10-13 12:20:54 -0400

    add wikidata and category

--- locations/spiders/welcome.py
+++ locations/spiders/welcome.py
@@ -1,11 +1,12 @@
 from scrapy.spiders import SitemapSpider
 
+from locations.categories import Categories
 from locations.structured_data_spider import StructuredDataSpider
 
 
 class WelcomeGB(SitemapSpider, StructuredDataSpider):
     name = "welcome"
-    item_attributes = {"brand": "Welcome"}
+    item_attributes = {"brand": "Welcome", "brand_wikidata": "Q123004215", "extras": Categories.SHOP_CONVENIENCE.value}
     sitemap_urls = ["https://stores.welcome-stores.co.uk/sitemap.xml"]
     sitemap_rules = [
         (
